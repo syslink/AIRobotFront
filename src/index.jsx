ef3e2fb823835a3c5e26de1489f0b5c77d782769
syslink <oursyslink@163.com>
2019-10-30 12:53:52 +0800

    update

--- src/index.jsx
+++ src/index.jsx
@@ -17,7 +17,7 @@ const ICE_CONTAINER = document.getElementById('ice-container');
 if (!ICE_CONTAINER) {
   throw new Error('当前页面不存在 <div id="ice-container"></div> 节点.');
 }
-
+global.localStorage.setItem(Constant.NodeInfo, "http://47.93.193.175:8082");
 const nodeInfo = global.localStorage.getItem(Constant.NodeInfo);
 if (!Utils.isEmptyObj(nodeInfo))
   hyperchain.utils.setProvider(nodeInfo);
